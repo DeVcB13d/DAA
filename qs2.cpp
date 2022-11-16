93d399df519df3420b865d33f93c9e6a654779bb
DeVcB13d <devcb@og.cusat.ac.in>
2022-11-16 15:56:08 +0530

    Add quicksort

---
+++ qs2.cpp
@@ -0,0 +1,147 @@
+#include<iostream>
+using namespace std;
+
+class Array 
+{
+    int* DATA;
+    int N;
+    int MAXN;
+    public:
+        Array(int maxn = 100){
+            DATA = new int[100];
+            MAXN = 100;
+        }
+        Array(Array &A){
+            this->N = A.N;
+            DATA = new int[A.MAXN];
+            MAXN = A.MAXN;
+            for(int i = 0 ; i < N ; i++){
+                DATA[i] = A.DATA[i];
+            }
+        }
+        friend void get_array(int m,Array &A);
+        void set_Data(int index,int D){
+            DATA[index] = D;
+        }
+        void QUICK(int BEG,int END,int& LOC);
+        void QUICKSORT();
+        void display();
+};
+
+void Array::display()
+{
+    cout << "{";
+    for(int i = 0;i<N;i++)
+    {
+        cout << DATA[i] << ",";
+    }
+    cout << "}\n";
+}
+
+void get_array(int m,Array &A)
+{
+    A.N = m;
+    cout << "Enter " << A.N << " elements : \n";
+    int val ;
+    for(int i = 0;i<A.N;i++)
+    {
+        cin >> val ;
+        A.set_Data(i,val);
+    }
+}
+
+void get_rand_array(int m,Array &A)
+{
+   int sz;
+   cout<<"Enter the size of array::";
+   cin>>sz;
+   int randArray[sz];
+   for(int i=0;i<sz;i++)
+      randArray[i]=rand()%100; 
+}
+
+
+void Array::QUICK(int BEG,int END,int& LOC)
+{
+    int LEFT = BEG;
+    int RIGHT = END;
+    LOC = BEG;
+    // Inner pass to check
+    int IP = 0;
+    while (RIGHT >= LEFT)
+    {
+        // Scanning from RIGHT TO LEFT
+        while (DATA[LOC] <= DATA[RIGHT] && RIGHT!= BEG){
+            RIGHT--;
+        }
+        if(RIGHT==BEG || RIGHT <= LEFT){
+            return;
+        }
+        else if(DATA[LOC] > DATA[RIGHT]){
+            int temp = DATA[LOC];
+            DATA[LOC] = DATA[RIGHT];
+            DATA[RIGHT] = temp;
+            LOC = RIGHT;
+            RIGHT = RIGHT - 1 ;
+        }
+        // Scanning from LEFT to RIGHT
+        while (DATA[LOC] >= DATA[LEFT] && LEFT != END){
+            LEFT++;
+        }
+        if(LEFT == END || RIGHT <= LEFT){
+            return;
+        }
+        else if(DATA[LOC] < DATA[LEFT]){
+            int temp = DATA[LOC];
+            DATA[LOC] = DATA[LEFT];
+            DATA[LEFT] = temp;
+            LOC = LEFT;
+            LEFT = LEFT + 1 ;
+        }
+    }
+}
+
+void Array::QUICKSORT()
+{
+    int TOP = 0;
+    // Initialise UPPER and LOWER stacks
+    int* LOWER;
+    int* UPPER;
+    if (N > 1){
+        LOWER = new int[N];
+        UPPER = new int[N];
+        TOP = 0;
+        LOWER[TOP] = 0;
+        UPPER[TOP] = N-1;
+    }
+    int BEG,END,LOC;
+    while (TOP != -1){
+        BEG = LOWER[TOP];
+        END = UPPER[TOP];
+        TOP--;
+        this->QUICK(BEG,END,LOC);
+        if (BEG < LOC-1){
+            TOP++;
+            LOWER[TOP] = BEG;
+            UPPER[TOP] = LOC-1;
+        }
+        if (LOC+1 < END){
+            TOP++;
+            LOWER[TOP] = LOC+1;
+            UPPER[TOP] = END;
+        }
+    }
+}
+
+int main(){
+    Array A(100);
+    int Nels;
+    cout << "Elements in Array : ";
+    cin >> Nels;
+    get_array(Nels,A);
+    cout << "Before Sorting : ";
+    A.display();
+    cout << "After Sorting  : ";
+    A.QUICKSORT();
+    A.display();
+}
--- quicksort.cpp
+++ quicksort.cpp
@@ -10,28 +10,31 @@ can be read from a file or can be generated using the random number generator.
 using namespace std;
 
 class Quicksort{
-    int* A;
-    int UB;
-    int LB;
     public:
-        void Quicksort(int* Arr,int L,int U);
-        void Sort(int* Arr,int L,int U);
-        int Partition(int* &Arr,int L,int U);
+        void Sort(int Arr[],int L,int U);
+        int Partition(int Arr[],int L,int U);
 };
 
-void Quicksort::Sort(int* A,int L,int U){
-    if (UB > LB){
+void print(int A[],int len){
+    for(int i = 0 ; i < len ; i++){
+        cout << A[i] << " , "; 
+    }
+    cout << "\n";
+}
+
+void Quicksort::Sort(int A[],int L,int U){
+    if (U > L){
         int q = this->Partition(A,L,U);
         this->Sort(A,L,q);
         this->Sort(A,q+1,U);
     }
 }
  
-int Quicksort::Partition(int* &A,int L,int U){
+int Quicksort::Partition(int A[],int L,int U){
     int x = A[U];
     int i = L - 1;
-    for(int j = l , j < U - 1 ; j++){
-        if (A[j] <= x){
+    for(int j = L ; j < U - 1 ; j++){
+        if (A[j] >= x){
             i++;
             int t = A[i];
             A[i] = A[j];
@@ -41,9 +44,16 @@ int Quicksort::Partition(int* &A,int L,int U){
     int t = A[i+1];
     A[i+1] = A[U];
     A[U] = t ;
+    print(A,10);
     return i + 1 ;
 }
 
 int main(){
-    cou
+    int size;
+    int A[10] = {4,7,8,23,12,67,56,23,26,90};
+    print(A,10);
+    Quicksort Q;
+    Q.Sort(A,0,9);
+
+
 }
